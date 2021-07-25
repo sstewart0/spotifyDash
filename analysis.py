03010ec7abd728df6a08619ad4cfaccc375b877d
sstewart0 <stewart.stephen.12@gmail.com>
2021-07-25 10:24:07 +0100

    removed redundant files

--- analysis.py
+++ analysis.py
@@ -268,7 +268,7 @@ def main():
 
     stream_data = get_data()
     save_data(stream_data, "./assets/stream_data.csv")
-    top_songs = get_top_songs(stream_data)
+    #top_songs = get_top_songs(stream_data)
     """
     save_data(top_songs, "./assets/top_songs.csv")
     # save top song artwork
@@ -278,14 +278,14 @@ def main():
         fp = './assets/artwork{i}.png'.format(i=num)
         save_image(img, fp)
         num += 1"""
-    top_artists = get_top_artists(stream_data)
+    #top_artists = get_top_artists(stream_data)
     """num = 0
     for index, row in top_artists.iterrows():
         img = get_artist_picture(row[0])
         fp = './assets/artist{i}.png'.format(i=num)
         save_image(img, fp)
         num += 1"""
-    genres = {}
+    """genres = {}
     for index, row in top_songs.head(1000).iterrows():
         genre = get_song_genre(row)
         print(genre)
@@ -296,7 +296,7 @@ def main():
                 genres[genre] = 1
 
     genre_data = pd.DataFrame.from_dict(genres)
-    save_data(genre_data, "./assets/genre_data.csv")
+    save_data(genre_data, "./assets/genre_data.csv")"""
     #save_data(top_artists, "./assets/top_artists.csv")
 
 
